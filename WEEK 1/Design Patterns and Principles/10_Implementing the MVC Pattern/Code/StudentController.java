caa6e3f6c809357d551ded9c248ed4aa68969999
ASVATHAMA <asvathsk@gmail.com>
2026-06-25 13:53:37 +0530

    Add files via upload

---
+++ WEEK 1/Design Patterns and Principles/10_Implementing the MVC Pattern/Code/StudentController.java
@@ -0,0 +1,30 @@
+public class StudentController {
+
+	Student student;
+	StudentView view;
+
+	StudentController(Student student, StudentView view) {
+		this.student = student;
+		this.view = view;
+	}
+
+	public void setStudentName(String name) {
+		student.setName(name);
+	}
+
+	public String getStudentName() {
+		return student.getName();
+	}
+
+	public void setStudentId(String id) {
+		student.setId(id);
+	}
+
+	public void setStudentGrade(String grade) {
+		student.setGrade(grade);
+	}
+
+	public void updateView() {
+		view.displayStudentDetails(student.getName(), student.getId(), student.getGrade());
+	}
+}
