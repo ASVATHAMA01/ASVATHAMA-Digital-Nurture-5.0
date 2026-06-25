51eb2c5c1aa82e9627c6ab52732edec5abd5bb50
ASVATHAMA <asvathsk@gmail.com>
2026-06-25 15:59:34 +0530

    Delete WEEK 1 directory

--- WEEK 1/Design Patterns and Principles/04_Implementing the Adapter Pattern/Code/AdapterTest.java
+++
@@ -1,9 +0,0 @@
-public class AdapterTest {
-    public static void main(String[] args) {
-        PaymentProcessor paypal = new PaypalAdapter(new PaypalGateway());
-        PaymentProcessor stripe = new StripeAdapter(new StripeGateway());
-
-        paypal.processPayment(100.00);
-        stripe.processPayment(250.00);
-    }
-}
